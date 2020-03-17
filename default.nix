9e13298cc64a72f1dae529840b6c4d4c12886e6b
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2020-03-17 14:46:31 +0100

    nix build

---
+++ default.nix
@@ -0,0 +1,49 @@
+{ system ? builtins.currentSystem
+, crossSystem ? null
+# allows to cutomize haskellNix (ghc and profiling, see ./nix/haskell.nix)
+, config ? {}
+# override scripts with custom configuration
+, customConfig ? {}
+# allows to override dependencies of the project without modifications,
+# eg. to test build against local checkout of nixpkgs and iohk-nix:
+# nix build -f default.nix cardano-node --arg sourcesOverride '{
+#   iohk-nix = ../iohk-nix;
+# }'
+, sourcesOverride ? {}
+# pinned version of nixpkgs augmented with overlays (iohk-nix and our packages).
+, pkgs ? import ./nix { inherit system crossSystem config sourcesOverride; }
+, gitrev ? pkgs.iohkNix.commitIdFromGitRepoOrZero ./.git
+}:
+with pkgs; with commonLib;
+let
+
+  haskellPackages = recRecurseIntoAttrs
+    # the Haskell.nix package set, reduced to local packages.
+    (selectProjectPackages cardanoBenchmarkingHaskellPackages);
+
+  self = {
+    inherit haskellPackages;
+
+    inherit (haskellPackages.cardano-tx-generator.identifier) version;
+    # Grab the executable component of our package.
+    inherit (haskellPackages.cardano-tx-generator.components.exes)
+      cardano-tx-generator;
+
+    inherit (pkgs.iohkNix) checkCabalProject;
+
+    # `tests` are the test suites which have been built.
+    tests = collectComponents' "tests" haskellPackages;
+    # `benchmarks` (only built, not run).
+    benchmarks = collectComponents' "benchmarks" haskellPackages;
+
+    checks = recurseIntoAttrs {
+      # `checks.tests` collect results of executing the tests:
+      tests = collectChecks haskellPackages;
+    };
+
+    shell = import ./shell.nix {
+      inherit pkgs;
+      withHoogle = true;
+    };
+};
+in self
---
+++ release.nix
@@ -0,0 +1,67 @@
+############################################################################
+#
+# Hydra release jobset.
+#
+# The purpose of this file is to select jobs defined in default.nix and map
+# them to all supported build platforms.
+#
+############################################################################
+
+# The project sources
+{ cardano-benchmarking ? { outPath = ./.; rev = "abcdef"; }
+
+# Function arguments to pass to the project
+, projectArgs ? {
+    config = { allowUnfree = false; inHydra = true; };
+    gitrev = cardano-benchmarking.rev;
+  }
+
+# The systems that the jobset will be built for.
+, supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+
+# The systems used for cross-compiling
+, supportedCrossSystems ? [ "x86_64-linux" ]
+
+# A Hydra option
+, scrubJobs ? true
+
+# Dependencies overrides
+, sourcesOverride ? {}
+
+# Import pkgs, including IOHK common nix lib
+, pkgs ? import ./nix { inherit sourcesOverride; }
+
+}:
+
+with (import pkgs.iohkNix.release-lib) {
+  inherit pkgs;
+  inherit supportedSystems supportedCrossSystems scrubJobs projectArgs;
+  packageSet = import cardano-benchmarking;
+  gitrev = cardano-benchmarking.rev;
+};
+
+with pkgs.lib;
+
+let
+
+  testsSupportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+  # Recurse through an attrset, returning all test derivations in a list.
+  collectTests' = ds: filter (d: elem d.system testsSupportedSystems) (collect isDerivation ds);
+  # Adds the package name to the test derivations for windows-testing-bundle.nix
+  # (passthru.identifier.name does not survive mapTestOn)
+  collectTests = ds: concatLists (
+    mapAttrsToList (packageName: package:
+      map (drv: drv // { inherit packageName; }) (collectTests' package)
+    ) ds);
+
+  sources = import ./nix/sources.nix;
+
+  jobs = {
+    native = mapTestOn (__trace (__toJSON (packagePlatforms project)) (packagePlatforms project));
+  } // (mkRequiredJob (
+      collectTests jobs.native.checks ++
+      collectTests jobs.native.benchmarks ++ [
+
+    ]));
+
+in jobs
