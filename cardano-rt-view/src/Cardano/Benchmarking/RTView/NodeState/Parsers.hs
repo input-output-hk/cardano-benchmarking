cfc12182e5f0f596657bbd71cfbce1f1edaa96b6
Denis Shevchenko <denis.shevchenko@iohk.io>
2020-05-16 19:17:21 +0400

    CAD-973: Receive complete peers information.

--- cardano-rt-view/src/Cardano/Benchmarking/RTView/NodeState/Parsers.hs
+++ cardano-rt-view/src/Cardano/Benchmarking/RTView/NodeState/Parsers.hs
@@ -3,7 +3,6 @@
 
 module Cardano.Benchmarking.RTView.NodeState.Parsers
     ( extractPeersInfo
-    , updateCurrentPeersInfo
     ) where
 
 import           Cardano.Prelude
@@ -13,8 +12,6 @@ import           Data.Aeson
                    ( Object
                    , (.:)
                    )
-import           Data.List
-                   ( last )
 import qualified Data.Text as T
 
 import           Cardano.Benchmarking.RTView.NodeState.Types
@@ -23,36 +20,32 @@ import           Cardano.Benchmarking.RTView.NodeState.Types
 extractPeersInfo :: Object -> [PeerInfo]
 extractPeersInfo peersObj =
   case result of
-    A.Success connectedPeers ->
-      let addrs = map (extractRemoteAddr . peer) $ peers connectedPeers
-      in [PeerInfo (T.unpack addr) "" "" | addr <- addrs]
+    A.Success (ConnectedPeers cPeers) ->
+      flip map cPeers $ \p ->
+        PeerInfo
+          { piEndpoint   = T.unpack $ peerAddress p
+          , piBytesInF   = T.unpack $ peerBytesInF p
+          , piReqsInF    = T.unpack $ peerReqsInF p
+          , piBlocksInF  = T.unpack $ peerBlocksInF p
+          , piSlotNumber = T.unpack $ peerSlotNo p
+          , piStatus     = T.unpack $ peerStatus p
+          }
     A.Error _ -> []
  where
   result :: A.Result ConnectedPeers
   result = A.fromJSON $ A.Object peersObj
 
-  -- We know that 'peer' contains 'Show'-result of 'ConnectionId'.
-  extractRemoteAddr :: Text -> Text
-  extractRemoteAddr = T.init . last . T.words
+-- Types for decoding from JSON-representation.
 
--- | Check if such peer already connected, and if so - take its slotNum and blockNum.
-updateCurrentPeersInfo :: [PeerInfo] -> [PeerInfo] -> [PeerInfo]
-updateCurrentPeersInfo currentPeersInfo [] = currentPeersInfo
-updateCurrentPeersInfo currentPeersInfo newPeersInfo =
-  flip map newPeersInfo $ \newPI@(PeerInfo connId _ _) ->
-    let existingPI = find (\(PeerInfo cId _ _) -> cId == connId) currentPeersInfo
-    in case existingPI of
-         Nothing -> newPI
-         Just pI -> PeerInfo connId (piSlotNumber pI) (piBlockNumber pI)
-
--- Types for decoding from JSON-representation. The only information we need here is ConnectionId.
-
-data ConnectedPeers = ConnectedPeers
-  { peers :: ![ConnectedPeer]
-  }
+newtype ConnectedPeers = ConnectedPeers [ConnectedPeer]
 
 data ConnectedPeer = ConnectedPeer
-  { peer :: !Text
+  { peerBytesInF  :: !Text
+  , peerReqsInF   :: !Text
+  , peerBlocksInF :: !Text
+  , peerAddress   :: !Text
+  , peerSlotNo    :: !Text
+  , peerStatus    :: !Text
   }
 
 instance A.FromJSON ConnectedPeers where
@@ -61,4 +54,9 @@ instance A.FromJSON ConnectedPeers where
 
 instance A.FromJSON ConnectedPeer where
   parseJSON = A.withObject "ConnectedPeer" $ \v -> ConnectedPeer
-    <$> v .: "peer"
+    <$> v .: "peerBytesInF"
+    <*> v .: "peerReqsInF"
+    <*> v .: "peerBlocksInF"
+    <*> v .: "peerAddress"
+    <*> v .: "peerSlotNo"
+    <*> v .: "peerStatus"
