53695a3e7d624f9807875d4165f08c837e5be362
Alexander Diemand <codieplusplus@apax.net>
2020-06-11 09:19:56 +0200

    CAD-1233 updated build environment and dependencies
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- benchmarks/chain-sync/benchmark-chain-sync-mainnet.sh
+++ benchmarks/chain-sync/benchmark-chain-sync-mainnet.sh
@@ -12,11 +12,6 @@ fi
 mkdir -p $DATADIR
 cd $DATADIR
 
-# remove blockchain
-if [ -d db-mainnet-0 ]; then
-  rm -rf db-mainnet-0
-fi
-
 # remove old log files
 rm node-0*
 
@@ -39,7 +34,7 @@ timeout ${TIME_LIMIT} ${NODE} \
   run \
   --config ${BASEPATH}/configuration/log-configuration.yaml \
   --database-path ./db-mainnet \
-  --topology ${BASEPATH}/configuration/topology-local.yaml \
+  --topology ${BASEPATH}/configuration/topology-byron-proxy.yaml \
   --socket-path /tmp/socket-bm-chain-sync \
   --host-addr 127.0.0.1 \
   --port 7778 \
@@ -48,9 +43,9 @@ timeout ${TIME_LIMIT} ${NODE} \
 
 #  --genesis-file ${BASEPATH}/configuration/mainnet-genesis.json \
 #  --genesis-hash "5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb" \
+#  --topology ${BASEPATH}/configuration/topology-local.yaml \
 #  --topology ${BASEPATH}/configuration/topology-byron-proxy.yaml \
 # "+RTS -T -I0 -N2 -A16m -RTS"
 
 cd ..
 ${BASEPATH}/analyse-logs.sh
-
