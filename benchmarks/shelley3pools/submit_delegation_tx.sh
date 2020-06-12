2ef0a8bebd632421d28808b2912579573272d7a8
Alexander Diemand <codieplusplus@apax.net>
2020-06-12 10:43:42 +0200

    CAD-1201 restore shelley3pools benchmark (#89)
    restored

---
+++ benchmarks/shelley3pools/submit_delegation_tx.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(realpath $(dirname "$0"))
+. ${BASEDIR}/../../scripts/common.sh
+. ${BASEDIR}/configuration/parameters
+
+CLICMD=${CLICMD:-"stack --nix exec cardano-cli --"}
+
+# === submit delegation transactions ===
+
+for N in ${STAKEPOOLS}; do
+  if [ -e ${GENESISDIR}/node${N}/tx-delegate${N}.tx ]; then
+    CARDANO_NODE_SOCKET_PATH=logs/sockets/${N} \
+    ${CLICMD} shelley transaction submit \
+                --tx-file ${GENESISDIR}/node${N}/tx-delegate${N}.tx \
+                --testnet-magic ${MAGIC}
+  else
+    echo "no delegation transaction ${GENESISDIR}/node${N}/tx-delegate${N}.tx found!"
+  fi
+done
