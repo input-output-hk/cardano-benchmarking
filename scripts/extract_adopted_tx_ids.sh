cb68061fae8d30c6d4d506153f19a1d65e6a7496
Alexander Diemand <codieplusplus@apax.net>
2020-06-16 23:22:49 +0200

    time to block: scripts to extract tx
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

---
+++ scripts/extract_adopted_tx_ids.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# arguments: node# [logs/node[0-9]-*.json]
+
+NODENUM=$1
+shift
+
+TEMPFILE=$(mktemp)
+
+  # grep from all log files passed in as arguments
+grep -h '"TraceAdoptedBlock"' $* |
+  # reformat string of tx ids as list
+  sed -e 's/"txid: TxId {_TxId = \([0-9a-f]\+\)}"\(,\{0,1\}\)/"\1"\2/g' | 
+  # reformat 'HashHeader'
+  sed -e 's/HashHeader {unHashHeader = \([a-z0-9]\+\)}/\1/g' |
+  # extract specific data
+  jq -cr 'select(.data.kind=="TraceAdoptedBlock") | [ .data.slot, .at, .data.kind, .data."block hash", .data."block size", (.data."tx ids" | length), .data."tx ids" ]' |
+  # reformat timestamp
+  sed -e 's/\([0-9-]\+\)T\([0-9:.]\+\)Z/\1 \2/' > ${TEMPFILE}
+
+OLDIFS=$IFS
+cat "${TEMPFILE}" | {
+  read ln
+  while [ -n "${ln}" ]; do
+
+    SLOT=$(echo $ln | sed -ne 's/^\[\([0-9]\+\),"\([^"]\+\)","[^"]*","[^"]*",[0-9]\+,[0-9]\+,\[\(.*\)\]\]$/\1/p')
+    TSTAMP=$(echo $ln | sed -ne 's/^\[\([0-9]\+\),"\([^"]\+\)","[^"]*","[^"]*",[0-9]\+,[0-9]\+,\[\(.*\)\]\]$/\2/p')
+    TXIDS=$(echo $ln | sed -ne 's/^\[\([0-9]\+\),"\([^"]\+\)","[^"]*","[^"]*",[0-9]\+,[0-9]\+,\[\(.*\)\]\]$/\3/p')
+    if [ -n "$TXIDS" ]; then
+      IFS=","
+      for txid in ${TXIDS}; do
+        echo "$NODENUM, $SLOT, \"$TSTAMP\", $txid"
+      done
+    fi
+
+    IFS=$OLDIFS
+    read ln
+  done
+}
+
