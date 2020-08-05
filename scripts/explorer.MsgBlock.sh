611e2804de75e14ac853991cfcf9911fef67aaa3
Kosyrev Serge <serge.kosyrev@iohk.io>
2020-08-05 17:07:08 +0300

    CAD-1455 analysis scripts:  update to node log format changes

--- scripts/explorer.MsgBlock.sh
+++ scripts/explorer.MsgBlock.sh
@@ -9,8 +9,8 @@ jq 'def katip_timestamp_to_iso8601:
         as $date_iso
       | { date_iso:    $date_iso
         , timestamp:   $date_iso | fromdateiso8601
-        , blkid:       .data.msg."block hash"
-        , txs:         (.data.msg."tx ids" | length)
+        , blkid:       .data.msg.blockHash
+        , txs:         (.data.msg.txIds | length)
         }
       )
     | sort_by (.timestamp)
