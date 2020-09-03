fd7567781f22c24389729cd74f1a1e23fff899c8
Denis Shevchenko <denis.shevchenko@iohk.io>
2020-09-03 10:16:40 +0400

    CAD-1398: RTView downloadable installers (Windows, Linux, macOS).
    CAD-1398: Nightly build for installers.

---
+++ nix/darwin-release.nix
@@ -0,0 +1,41 @@
+############################################################################
+# Darwin release cardano-rt-view-service-*.zip
+#
+# This bundles up the macOS executable with its dependencies
+# and static directory. Result is *.zip file.
+#
+############################################################################
+
+{ pkgs
+, releaseVersion
+, exes
+, staticDir
+}:
+
+let
+  lib = pkgs.lib;
+  name = "cardano-rt-view-service-${releaseVersion}-darwin";
+  rtViewServiceExe = lib.head (lib.filter (exe: lib.hasInfix "cardano-rt-view-service" exe.name) exes);
+
+in pkgs.runCommand name {
+    buildInputs = with pkgs.buildPackages; [
+      haskellBuildUtils.package
+      zip
+    ];
+  } ''
+  mkdir -p $out release
+  cd release
+
+  cp -n --remove-destination -v ${rtViewServiceExe}/bin/* ./
+
+  mkdir ./static
+  cp -R ${staticDir}/* ./static/
+
+  chmod -R +w .
+
+  zip -r $out/${name}.zip .
+
+  dist_file=$(ls $out)
+  mkdir -p $out/nix-support
+  echo "file binary-dist $out/$dist_file" > $out/nix-support/hydra-build-products
+''
---
+++ nix/windows-release.nix
@@ -0,0 +1,41 @@
+############################################################################
+# Windows release cardano-rt-view-service-*.zip
+#
+# This bundles up the windows executable with its dependencies
+# and static directory.
+#
+############################################################################
+
+{ pkgs
+, releaseVersion
+, exes
+, staticDir
+}:
+
+let
+  lib = pkgs.lib;
+  name = "cardano-rt-view-service-${releaseVersion}-win64";
+  rtViewServiceExe = lib.head (lib.filter (exe: lib.hasInfix "cardano-rt-view-service" exe.name) exes);
+
+in pkgs.runCommand name {
+    buildInputs = with pkgs.buildPackages; [
+      zip
+      haskellBuildUtils.package
+    ];
+  } ''
+  mkdir -p $out release
+  cd release
+
+  cp -n --remove-destination -v ${rtViewServiceExe}/bin/* ./
+
+  mkdir ./static
+  cp -R ${staticDir}/* ./static/
+
+  chmod -R +w .
+
+  zip -r $out/${name}.zip .
+
+  dist_file=$(ls $out)
+  mkdir -p $out/nix-support
+  echo "file binary-dist $out/$dist_file" > $out/nix-support/hydra-build-products
+''
