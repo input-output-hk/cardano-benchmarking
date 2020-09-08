1854a0c10703aa0db48ca75f6bde888fc11537bb
Denis Shevchenko <denis.shevchenko@iohk.io>
2020-09-09 00:10:41 +0400

    CAD-1786: RTView interactive dialog

--- cardano-rt-view/src/Cardano/Benchmarking/RTView.hs
+++ cardano-rt-view/src/Cardano/Benchmarking/RTView.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.Benchmarking.RTView
@@ -9,19 +8,17 @@ import           Cardano.Prelude hiding (newMVar)
 
 import           Control.Concurrent.Async (async, waitAnyCancel)
 import           Control.Concurrent.MVar.Strict (MVar, newMVar)
-import           Data.List (nub, nubBy)
-import qualified System.Exit as Ex
+import qualified Data.Text.IO as TIO
 
 import           Cardano.BM.Backend.Switchboard (addUserDefinedBackend)
-import           Cardano.BM.Configuration (Configuration, getAcceptAt, setup)
 import           Cardano.BM.Data.Backend (Backend (..))
-import           Cardano.BM.Data.Configuration (RemoteAddr (..), RemoteAddrNamed (..))
 import qualified Cardano.BM.Setup as Setup
 import           Cardano.BM.Trace (Trace, logNotice)
 import           Cardano.BM.Tracing (appendName)
 
 import           Cardano.Benchmarking.RTView.Acceptor (launchMetricsAcceptor)
 import           Cardano.Benchmarking.RTView.CLI (RTViewParams (..))
+import           Cardano.Benchmarking.RTView.Config (prepareConfigAndParams)
 import           Cardano.Benchmarking.RTView.ErrorBuffer (ErrorBuffer, effectuate, realize,
                                                           unrealize)
 import           Cardano.Benchmarking.RTView.NodeState.Types (NodesState, defaultNodesState)
@@ -30,10 +27,12 @@ import           Cardano.Benchmarking.RTView.Server (launchServer)
 
 -- | Run the service.
 runCardanoRTView :: RTViewParams -> IO ()
-runCardanoRTView params = do
-  config <- readConfig (rtvConfig params)
-  acceptors <- checkIfTraceAcceptorIsDefined config (rtvConfig params)
-  makeSureTraceAcceptorsAreUnique acceptors
+runCardanoRTView params' = do
+  TIO.putStrLn "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+  TIO.putStrLn "┃ RTView: real-time view service for cardano-node ┃"
+  TIO.putStrLn "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+
+  (config, params, acceptors) <- prepareConfigAndParams params'
 
   (tr :: Trace IO Text, switchBoard) <- Setup.setupTrace_ config "cardano-rt-view"
   let accTr = appendName "acceptor" tr
@@ -60,49 +59,3 @@ runCardanoRTView params = do
   serverThr   <- async $ launchServer nodesStateMVar params acceptors
 
   void $ waitAnyCancel [acceptorThr, updaterThr, serverThr]
-
--- | Reads the service' configuration file (path is passed via '--config' CLI option).
-readConfig :: FilePath -> IO Configuration
-readConfig pathToConfig = setup pathToConfig `catch` exceptHandler
- where
-  exceptHandler :: IOException -> IO Configuration
-  exceptHandler e =
-    Ex.die $ "Exception while reading configuration "
-             <> pathToConfig
-             <> ", exception: "
-             <> show e
-
--- | RTView service requires at least one |TraceAcceptor|.
-checkIfTraceAcceptorIsDefined
-  :: Configuration
-  -> FilePath
-  -> IO [RemoteAddrNamed]
-checkIfTraceAcceptorIsDefined config pathToConfig =
-  getAcceptAt config >>= \case
-    Just acceptors -> return acceptors
-    Nothing -> Ex.die $ "No trace acceptors found in the configuration: " <> pathToConfig
-
--- | If configuration contains more than one trace acceptor,
---   check if they are unique, to avoid socket problems.
-makeSureTraceAcceptorsAreUnique
-  :: [RemoteAddrNamed]
-  -> IO ()
-makeSureTraceAcceptorsAreUnique acceptors = do
-  checkIfNodesNamesAreUnique
-  checkIfNetParametersAreUnique
- where
-  checkIfNodesNamesAreUnique =
-    when (length names /= length (nub names)) $
-      Ex.die "Nodes' names in trace acceptors must be unique!"
-
-  checkIfNetParametersAreUnique =
-    when (length addrs /= length (nubBy compareNetParams addrs)) $
-      Ex.die "Nodes' network parameters in trace acceptors must be unique!"
-
-  compareNetParams (RemoteSocket h1 p1) (RemoteSocket h2 p2) = h1 == h2 && p1 == p2
-  compareNetParams (RemoteSocket _ _)   (RemotePipe _)       = False
-  compareNetParams (RemotePipe _)       (RemoteSocket _ _)   = False
-  compareNetParams (RemotePipe p1)      (RemotePipe p2)      = p1 == p2
-
-  names = [name | RemoteAddrNamed name _ <- acceptors]
-  addrs = [addr | RemoteAddrNamed _ addr <- acceptors]
