dd5e5b8c59edb779123a9ece0dfa050d41bc99d8
Kosyrev Serge <serge.kosyrev@iohk.io>
2020-10-08 19:10:08 +0300

    shelley3pools | tmux:  kill the "Shelley3Pools" session before starting

--- benchmarks/shelley3pools/start.sh
+++ benchmarks/shelley3pools/start.sh
@@ -5,9 +5,8 @@ set -e
 # this benchmark runs in 'tmux' which provides several windows and panes
 # to arrange the nodes and the transaction generator.
 
-# add to your ~/.tmux.conf:
-# set-window-option -g mouse on
-# set -g default-terminal "tmux-256color"
-
+if test -n "$(pgrep -fal tmux)"
+then tmux kill-session -t Shelley3Pools
+fi
 tmux new-s -E -s Shelley3Pools -n Main "/usr/bin/env bash ./benchmark.sh $*; $SHELL"
 
