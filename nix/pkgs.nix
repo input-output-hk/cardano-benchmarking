5fe24629ddea033c4c3e1cc0b8fc366956f41ae8
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-04-12 16:29:51 +0300

    CAD-2772 nix:  update ghc to 8.10.4

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,7 +1,7 @@
 # our packages overlay
 pkgs: _: with pkgs;
   let
-    compiler = config.haskellNix.compiler or "ghc8102";
+    compiler = config.haskellNix.compiler or "ghc8104";
   in {
   cardanoBenchmarkingHaskellPackages = import ./haskell.nix {
     inherit compiler
